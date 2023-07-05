9fdecf09da97807a2910585c1254718ea6f59e50
xakimov <xakimov.dev@gmail.com>
2023-07-05 10:29:00 +0500

    project is created

---
+++ src/main/java/com/webrtc/WebRtcAudioStreamingServerApplication.java
@@ -0,0 +1,13 @@
+package com.webrtc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebRtcAudioStreamingServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebRtcAudioStreamingServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/webrtc/WebRtcAudioStreamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webrtc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebRtcAudioStreamingServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
